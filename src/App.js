392de754f788b899e545b9ba10f64d4356a74239
Rahul <rahuljayaraman8@gmail.com>
2021-11-27 01:10:41 +0530

    nav bar added

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import './components/style/App.css';
 import Row from './components/Row';
 import requests from './requests';
 import Banner from './components/Banner';
-
+import Nav from './components/Nav';
 function App() {
   return (
     <div className="app">
+      <Nav />
       <Banner />
       <Row
         title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOrginals}
---
+++ src/components/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './style/Nav.css'
+
+function Nav() {
+    return (
+        <div className="nav">
+            <img className="nav_logo" src=" https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1198px-Netflix_2015_logo.svg.png" alt="Netflix logo" />
+        </div>
+    )
+}
+
+export default Nav
