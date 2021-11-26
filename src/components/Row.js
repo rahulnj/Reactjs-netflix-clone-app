22d74aa3ef57230cbdd0d833479807f1eac6eba9
Rahul <rahuljayaraman8@gmail.com>
2021-11-26 21:39:59 +0530

    api added

---
+++ src/components/Row.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react'
+import instance from '../axios';
+import '../Row.css'
+const base_url = "https://image.tmdb.org/t/p/original/"
+
+
+function Row({ title, fetchUrl }) {
+    const [movies, setMovies] = useState([]);
+    //A snippet of code that runs on a specific condition
+    useEffect(() => {
+        async function fetchData() {
+            const request = await instance.get(fetchUrl);
+            setMovies(request.data.results)
+            return request
+        }
+        fetchData();
+    }, [fetchUrl]);
+
+    console.log(movies);
+    return (
+        <div className="row">
+            <h2>{title}</h2>
+            <div className="row_posters">
+                {movies.map(movie => (
+                    <img
+                        key={movie.id}
+                        className="row_poster"
+                        src={`${base_url}${movie.poster_path}`}></img>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default Row
